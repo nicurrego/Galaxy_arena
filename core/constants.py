3afe60b95cf7beccb67cf1fe3b631c94009a8684
Nicolas Urrego <nicurrego@gmail.com>
2025-06-17 18:07:13 +0900

    feat(core): add constants.py with environment settings

---
+++ core/constants.py
@@ -0,0 +1,21 @@
+# Screen dimensions
+WIDTH = 720
+HEIGHT = 360
+
+# Colors (R, G, B)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+# Spaceship
+SPACESHIP_WIDTH = 55
+SPACESHIP_HEIGHT = 40
+
+# Game properties
+FPS = 60
+VEL = 5
+BORDER_WIDTH = 10
+
+# Assets path
+ASSETS_PATH = "assets"
