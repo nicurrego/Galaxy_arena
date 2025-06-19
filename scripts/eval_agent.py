fd563016addcfa617973c1f5989e1e9a82c9957d
Nicolas Urrego <nicurrego@gmail.com>
2025-06-19 10:02:09 +0900

    refactor(scripts): evaluate trained agent using latest saved model

--- scripts/eval_agent.py
+++ scripts/eval_agent.py
@@ -1,20 +1,29 @@
+import time
 import gymnasium as gym
 from stable_baselines3 import PPO
 from envs.galaxy_env import GalaxyEnv
 
 def main():
+    # Path to latest checkpoint
+    model_path = ".\models\ppo_galaxy_model_100000_steps.zip"
+
+    # Create env with rendering enabled
     env = GalaxyEnv(render_mode="human")
-    model = PPO.load("ppo_galaxy_model")
-    obs, _ = env.reset()
-    done = False
+    model = PPO.load(model_path, env=env)
 
-    while not done:
-        action, _states = model.predict(obs, deterministic=True)
-        obs, reward, terminated, truncated, info = env.step(action)
-        if terminated or truncated:
-            done = True
-        env.render()
-
+    episodes = 5
+    for ep in range(episodes):
+        obs, info = env.reset()
+        done = False
+        total_reward = 0
+        while not done:
+            action, _states = model.predict(obs)
+            obs, reward, terminated, truncated, info = env.step(action)
+            total_reward += reward
+            env.render()
+            time.sleep(0.03)
+            done = terminated or truncated
+        print(f"Episode {ep+1}: Total Reward = {total_reward}")
     env.close()
 
 if __name__ == "__main__":
