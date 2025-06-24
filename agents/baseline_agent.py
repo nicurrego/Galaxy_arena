6922845ca979e83ddb44e03e0ac84fe478fc0cfd
Nicolas Urrego <nicurrego@gmail.com>
2025-06-24 14:48:41 +0900

    fix(agents): stop 'always shooting'

--- agents/baseline_agent.py
+++ agents/baseline_agent.py
@@ -6,7 +6,4 @@ class BaselineAgent:
         pass
 
     def act(self, obs):
-        # Example: always shoot, otherwise move up/down randomly
-        if np.random.rand() < 0.3:
-            return Action.SHOOT
-        return np.random.choice([Action.UP, Action.DOWN, Action.LEFT, Action.RIGHT, Action.STAY])
+        return np.random.choice([Action.UP, Action.DOWN, Action.LEFT, Action.RIGHT, Action.STAY, Action.SHOOT])
